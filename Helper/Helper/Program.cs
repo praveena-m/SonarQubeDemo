3866e85185b74654a0456dc3ea12af06637e29eb
Ravish E <ravish@gesoftsys.com>
2018-12-06 18:56:01 +0530

    SonarQube refactoring

--- Helper/Helper/Program.cs
+++ Helper/Helper/Program.cs
@@ -1,31 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Helper
+﻿namespace Helper
 {
     class Program
     {
         static void Main(string[] args)
         {
             int aa = 1;
-            int b = aa / 23;
-            string password = "abc@123";
+           aa = aa / 23;
 
             string status = null;
             try
             {
                 status = GetStatus();
             }
-            catch (Exception)
+            catch 
             {
-
                 throw;
             }
 
-            string newStatus = status + " updated";
+            //string newStatus = status + " updated";
         }
 
         public static string GetStatus()
