67cd54bd38a93834244227ece95999527184d981
Ravish E <ravish@gesoftsys.com>
2018-12-06 19:00:58 +0530

    code refactor

--- Helper/Helper/Program.cs
+++ Helper/Helper/Program.cs
@@ -1,23 +1,25 @@
-﻿namespace Helper
+﻿using System;
+
+namespace Helper
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int aa = 1;
-           aa = aa / 23;
 
             string status = null;
             try
             {
                 status = GetStatus();
             }
-            catch 
+            catch (Exception ex)
             {
+                System.Diagnostics.Trace.WriteLine(ex.Message);
                 throw;
             }
 
-            //string newStatus = status + " updated";
+            status = status + " updated";
+            System.Diagnostics.Trace.WriteLine(status);
         }
 
         public static string GetStatus()
