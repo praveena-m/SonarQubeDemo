d695d717d6846ab456155f96c1e6c2080de79a7b
Ravish E <ravish@gesoftsys.com>
2018-12-06 17:48:27 +0530

    First code Commit

---
+++ Helper/Helper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aa = 1;
+            int b = aa / 23;
+        }
+    }
+}
